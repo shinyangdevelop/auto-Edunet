3602cd3dcfc1fabff5ab10a6d29b86e9c9171242
shinyangdevelop <shinyangdevelop@gmail.com>
2021-10-26 21:54:28 +0900

    Added code that clicks continue button when the video is done.

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
 import pyautogui
 
-print(pyautogui.pixelMatchesColor(1224, 334, (236, 240, 241)))
-print(pyautogui.pixelMatchesColor(1210, 337, (255, 255, 255)))
+# print(pyautogui.pixelMatchesColor(1224, 334, (236, 240, 241)))
+# print(pyautogui.pixelMatchesColor(1210, 337, (255, 255, 255)))
+
+while True:
+    if pyautogui.pixelMatchesColor(1224, 334, (236, 240, 241)) and pyautogui.pixelMatchesColor(1210, 337, (255, 255, 255)):
+        pyautogui.click(1224, 334)
+        break
