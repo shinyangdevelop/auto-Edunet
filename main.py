3026f9cd89c3cdf791201b8e5b83d79198997281
shinyangdevelop <shinyangdevelop@gmail.com>
2021-10-26 22:18:47 +0900

    Changed the code with locateOnScreen() function.

--- main.py
+++ main.py
@@ -6,6 +6,5 @@ import pyautogui
 while True:
     if pyautogui.locateOnScreen("images/x_button.png") is not None:
         pyautogui.click(pyautogui.locateOnScreen("images/x_button.png"))
-    if pyautogui.pixelMatchesColor(1224, 334, (236, 240, 241)) and pyautogui.pixelMatchesColor(1210, 337, (255, 255, 255)):
-        pyautogui.click(1224, 334)
-        break
+    if pyautogui.locateOnScreen("images/continue_button.png") is not None:
+        pyautogui.click(pyautogui.locateOnScreen("images/continue_button.png"))
