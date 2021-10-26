d8014f4985a30cdc97267f7bae8f73bd70ce8139
shinyangdevelop <shinyangdevelop@gmail.com>
2021-10-26 21:57:25 +0900

    Added code that clicks X button if the download bar at the bottom is activated.

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ import pyautogui
 # print(pyautogui.pixelMatchesColor(1210, 337, (255, 255, 255)))
 
 while True:
+    if pyautogui.locateOnScreen("images/x_button.png") is not None:
+        pyautogui.click(pyautogui.locateOnScreen("images/x_button.png"))
     if pyautogui.pixelMatchesColor(1224, 334, (236, 240, 241)) and pyautogui.pixelMatchesColor(1210, 337, (255, 255, 255)):
         pyautogui.click(1224, 334)
         break
